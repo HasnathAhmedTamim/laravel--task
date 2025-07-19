c6319ad96ae05f6824fe33c36ab72a5ed88d0019
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2025-07-20 00:05:16 +0600

    routes web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Laravel API Server is Running!',
+        'version' => '1.0.0',
+        'endpoints' => [
+            'User Registration' => 'POST /api/register',
+            'Blog Posts' => [
+                'Create' => 'POST /api/posts',
+                'Get All' => 'GET /api/posts',
+                'Get Single' => 'GET /api/posts/{id}',
+                'Update' => 'PUT /api/posts/{id}',
+                'Delete' => 'DELETE /api/posts/{id}'
+            ],
+            'Tasks' => [
+                'Create' => 'POST /api/tasks',
+                'Get All' => 'GET /api/tasks',
+                'Get Single' => 'GET /api/tasks/{id}',
+                'Update' => 'PUT /api/tasks/{id}',
+                'Delete' => 'DELETE /api/tasks/{id}'
+            ]
+        ],
+        'documentation' => 'See README.md for detailed API usage'
+    ]);
 });
